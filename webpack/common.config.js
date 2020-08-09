d559ecbcb6d83c970f1d74fad8b7e7450a76b3ae
Phong Tran <tranphongbb@icloud.com>
2020-08-09 18:57:34 +0700

    minor changes

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -58,9 +58,6 @@ module.exports = {
             {
                 test: /\.(jpg|png|gif|svg)$/,
                 loader: "image-webpack-loader",
-                // Specify enforce: 'pre' to apply the loader
-                // before url-loader/svg-url-loader
-                // and not duplicate it in rules with them
                 enforce: "pre",
             },
             {
